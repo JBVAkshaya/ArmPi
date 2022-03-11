25d08b00f05d475ebdaf4aa5d5242183a6fcd35c
JBVAkshaya <agrawaak@oregonstate.edu>
2022-03-11 14:20:08 -0800

    try a move

--- Functions/dance.py
+++ Functions/dance.py
@@ -164,11 +164,11 @@ if __name__ == "__main__":
 
     # Dummy coord to test with
     try:
-        mover.move_arm(0, 12, 20)
-        mover.move_arm(10, 12, 20)
-        mover.move_arm(10, 12, 12)
-        mover.move_arm(-10, 12, 12)
-        mover.move_arm(-10, 12, 20)
+        # mover.move_arm(0, 12, 20)
+        mover.move_arm(20, 12, 20)
+        mover.move_arm(20, 12, 12)
+        mover.move_arm(-20, 12, 12)
+        mover.move_arm(-20, 12, 20)
         mover.move_arm(0, 12, 20)
     except:
         print("does not work")
