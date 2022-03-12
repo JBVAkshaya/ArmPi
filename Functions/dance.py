607240213f2693274df7fa199d603a3e67bbeee1
JBVAkshaya <agrawaak@oregonstate.edu>
2022-03-11 19:55:39 -0800

    try a move

--- Functions/dance.py
+++ Functions/dance.py
@@ -268,7 +268,7 @@ if __name__ == "__main__":
     t1 = time.time()
     i = 0 
     while i<=3:
-        if time.time()-t1 <5:
+        if time.time()-t1 <2:
             status = no_motion(my_camera)
             key = cv2.waitKey(1)
             if key == 27:
