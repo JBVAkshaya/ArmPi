7b5b0108cd308d44d7ed38976f53a2a282f68cd1
JBVAkshaya <agrawaak@oregonstate.edu>
2022-03-11 16:48:19 -0800

    try a move

--- Functions/dance.py
+++ Functions/dance.py
@@ -252,7 +252,7 @@ if __name__ == "__main__":
     while True:
         # mover.move_arm(30, 12, 30)
         # mover.move_arm(30, 12, 12)
-        mover.move_arm(0, 12, 20)
+        mover.move_arm(0, -12, 20)
         mover.move_arm(0, 12, 30)
         # mover.move_arm(0, 12, 12)
         # mover.move_arm(-20, 12, 12)
